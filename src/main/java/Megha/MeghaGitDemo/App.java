70e406f4ba1e9b228e064f4d5e5e79b1d7d2f658
Meeghaa <meghagangavane69@gmail.com>
2023-02-24 14:43:18 +0530

    first commit

---
+++ src/main/java/Megha/MeghaGitDemo/App.java
@@ -0,0 +1,13 @@
+package Megha.MeghaGitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Regration/FirstProgramme.java
@@ -0,0 +1,10 @@
+package Regration;
+
+public class FirstProgramme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Megha GIt Demo !st Programme");
+	}
+
+}
